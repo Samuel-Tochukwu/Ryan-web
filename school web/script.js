26f86358ffa790d8ba2469e23ece241b03df2ad3
Samuel-Tochukwu <samueltochukwu2008@gmail.com>
2026-03-26 02:47:21 -0700

    Add files via upload

---
+++ school web/script.js
@@ -0,0 +1,11 @@
+function scrollToSection(id){
+document.getElementById(id).scrollIntoView({behavior:'smooth'});
+}
+
+const faders=document.querySelectorAll('.fade');
+window.addEventListener('scroll',()=>{
+faders.forEach(el=>{
+const top=el.getBoundingClientRect().top;
+if(top<window.innerHeight-100){el.classList.add('show');}
+});
+});
